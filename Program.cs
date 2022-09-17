1405625e2ddf35984cb512dceb3898627761f931
Cix8 <francesco.natal_fn@libero.it>
2022-09-17 14:21:00 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿void StampaArray(int[] array)
+{
+    Console.Write("[");
+    int i;
+    for(i = 0; i < array.Length - 1; i++) {
+        Console.Write(array[i]);
+        Console.Write(",");
+    }
+    Console.Write(array[i]);
+    Console.WriteLine("]");
+}
+
+int Quadrato(int numero)
+{
+    return Convert.ToInt32(Math.Pow(numero, 2));
+}
